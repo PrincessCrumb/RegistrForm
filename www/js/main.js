c8393f17397d793ef53e6333696776e9800f24d4
PrincessCrumb <121231819+PrincessCrumb@users.noreply.github.com>
2023-08-28 11:20:36 +0400

    Add files via upload

---
+++ www/js/main.js
@@ -0,0 +1,31 @@
+function checkForm(el) {
+
+var login = el.name.value;
+var pass = el.pass.value;
+var repass = el.repass.value;
+var state = el.state.value;
+
+var fail="";
+
+if ((login=="") || (pass=="")|| (repass=="") || (state=="")) {
+fail="Нужно заполнить все поля!";}
+else {
+if (pass!=repass)
+fail="Пароли должны совпадать!"}
+
+
+if(fail != ""){
+    document.getElementById('error').innerHTML= fail;
+    return false;}
+else {
+alert("Всё заполнено верно!");
+
+return true;}
+}
+
+
+
+
+
+
+
